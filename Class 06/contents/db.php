94458576ccffd1f4e9617e94702fe771bebf35e6
devriazul <engr.riazul@gmail.com>
2022-06-07 22:36:47 +0600

    Conection db normally

--- Class 06/contents/db.php
+++ Class 06/contents/db.php
@@ -1,18 +1,30 @@
 <?php
-$serverName = "localhost";
-$userName = "root";
-$passWord = "";
+// $serverName = "localhost";
+// $userName = "root";
+// $passWord = "";
 
-//Create connection
+// //Create connection
 
-$conn = new mysqli($serverName,$userName,$passWord);
+// $conn = new mysqli($serverName,$userName,$passWord);
 
-//check connection
+// //check connection
 
-if($conn->connect_error){
-    die("connection failed"). $conn->connect_error;
-}else{
-    "Connection Successfully!";
+// if($conn->connect_error){
+//     die("connection failed"). $conn->connect_error;
+// }else{
+//     "Connection Successfully!";
+// }
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=csphp", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-
 ?>
