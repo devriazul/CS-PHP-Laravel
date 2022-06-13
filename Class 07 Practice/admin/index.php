bf73af72b0b328d870dc9a008ebf7321f089d17c
devriazul <engr.riazul@gmail.com>
2022-06-13 19:43:24 +0600

    Git split done!

---
+++ Class 07 Practice/admin/index.php
@@ -0,0 +1,13 @@
+<?php 
+  require_once('include/header.php');
+  require_once('include/sidebar.php')
+?>
+  <div class="row">
+      <div class="col-md-12 main_content">
+          Welcome Mr. Xyz
+      </div>
+  </div>
+  <?php
+    require_once('include/footer.php')
+  
+  ?>
