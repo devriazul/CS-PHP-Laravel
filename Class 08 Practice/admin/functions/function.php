a6f00b4571ca29b8bb6fe512c0949c58ad680b48
devriazul <engr.riazul@gmail.com>
2022-06-14 22:17:22 +0600

    Database linked

--- Class 08 Practice/admin/functions/function.php
+++ Class 08 Practice/admin/functions/function.php
@@ -1,5 +1,7 @@
 <?php 
 
+require_once ('db.php');
+
 function get_header(){
     require_once('include/header.php');
 }
--- Class 08 Practice/admin/index.php
+++ Class 08 Practice/admin/index.php
@@ -3,6 +3,7 @@
   get_header();
   get_sidebar();
 ?>
+
   <div class="row">
       <div class="col-md-12 main_content">
           Welcome Mr. Xyz
