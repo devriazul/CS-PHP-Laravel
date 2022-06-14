9e2e4c477a39c3f9ac51210e4930aaabb643c95b
devriazul <engr.riazul@gmail.com>
2022-06-14 21:39:48 +0600

    Database connection

---
+++ Class 08 Practice/admin/functions/function.php
@@ -0,0 +1,13 @@
+<?php 
+
+function get_header(){
+    require_once('include/header.php');
+}
+function get_sidebar(){
+    require_once('include/sidebar.php');
+}
+function get_footer(){
+    require_once('include/footer.php');
+}
+
+?>
