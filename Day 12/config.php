eeddbd1ff8770cd24449ed525adc2cd30f5082a8
devriazul <engr.riazul@gmail.com>
2022-06-29 18:19:39 +0600

    Day 12 content added

---
+++ Day 12/config.php
@@ -0,0 +1,12 @@
+<?php
+  $db_name='university2201';
+  $db_user='root';
+  $db_pass='';
+  $db_host='localhost';
+
+  $con=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+
+  if(!$con){
+    echo "Database Connection Error!";
+  }
+?>
